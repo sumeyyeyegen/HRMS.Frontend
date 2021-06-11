153a9cf88e72553ab2da506283c820c8d2e7ee63
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-12 02:10:07 +0300

    footer updated.

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-
-    </div>
+    <footer className="bg-white d-flex justify-content-center align-items-center text-center border-top mt-5" style={{ height: "100px" }}>
+      <h1 className="font-italic text-muted">Footer</h1>
+    </footer >
   )
 }
 
