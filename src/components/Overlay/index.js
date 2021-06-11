3ca117586275e13da8ba9f839ca13ce786accea5
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-11 22:35:27 +0300

    overlay completed.

---
+++ src/components/Overlay/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import overlayImage from '../assets/images/Home_ Header.svg';
+import './Overlay.scss'
+
+function Overlay() {
+  return (
+    <div className="row">
+      <div className="col-sm">
+        <img src={overlayImage} alt="" className="img-overlay" />
+      </div>
+      <div className="col-sm mt-5 mt-sm-0">
+        <h1 className='fw-bold text-primary'>
+          Services Offered
+        </h1>
+        <ul>
+          <li>Job search by city</li>
+          <li>Job search by job title</li>
+          <li>All job advertisement list</li>
+          <li>Easy access to CV of candidates</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Overlay
