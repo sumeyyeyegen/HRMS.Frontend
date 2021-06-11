bc78386df9076b7d39bd4b555a5e1dda2425aa99
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-11 22:35:12 +0300

    overlay component added in Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Overlay from '../../components/Overlay/Overlay'
+
+function Home() {
+  return (
+    <div className="container">
+      <Overlay />
+    </div>
+  )
+}
+
+export default Home
