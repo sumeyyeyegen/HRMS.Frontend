cce63b7e22816c7fc1b86169603382a7e766bb59
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-11 22:18:37 +0300

    App.css to App.scss and constants added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Navbar from './layouts/Navbar/Navbar';
+import Footer from './layouts/Footer/Footer';
+
+import 'bootstrap';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App" style={{ height: "1000px" }}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
