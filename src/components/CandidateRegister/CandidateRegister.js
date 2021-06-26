4e1a1c2c8ff514d65d8df810cf9d96b81274e244
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-27 02:20:26 +0300

    candidateRegister added.

--- src/components/CandidateRegister/CandidateRegister.js
+++ src/components/CandidateRegister/CandidateRegister.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Form, Formik, Field } from 'formik'
+import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import KodlamaIoTextInput from '../../utilities/customFormControls/KodlamaIoTextInput'
+import { useToasts } from 'react-toast-notifications';
+import CandidatesService from '../../services/CandidatesService';
 
 function CandidateRegister() {
+
+  const { addToast } = useToasts();
+
   const initialValues = {
     firstName: "",
     lastName: "",
@@ -17,14 +22,20 @@ function CandidateRegister() {
     firstName: Yup.string().required("Lütfen isminizi giriniz"),
     lastName: Yup.string().required("Lütfen sayadınızı giriniz"),
     identityNumber: Yup.string().required("Lütfen Tc kimlik numaranızı giriniz"),
-    email: Yup.string().required("Lütfen email adresinizi giriniz"),
+    email: Yup.string().email("Geçersiz email formatı").required("Lütfen email adresinizi giriniz"),
     birthYear: Yup.number().required("Lütfen doğum yılınızı giriniz"),
     password: Yup.string().required("Lütfen şifrenizi giriniz"),
     passwordRepeat: Yup.string()
-      .oneOf([Yup.ref("password"), null], "Girilen şifreler farklı.")
-      .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", "En az sekiz karakter, en az bir büyük harf, bir küçük harf ve bir sayı içermelidir.")
+      .oneOf([Yup.ref("password"), ""], "Girilen şifreler farklı.")
       .required("Lütfen Şifrenizi Doğrulayın."),
   })
+
+  const onSubmit = (values) => {
+    let candidatesService = new CandidatesService();
+    candidatesService.registerCandidate(values).then(result => {
+      addToast(result.data.message, { appearance: result.data.success ? "success" : "error", autoDismiss: true });
+    })
+  }
 
   return (
     <div className="container">
@@ -34,22 +45,30 @@ function CandidateRegister() {
             initialValues={initialValues}
             //yup ile gelen özellik. zorunlu alanları girmemizi sağlar
             validationSchema={schema}
-            onSubmit={(values) => { console.log(values); }}
+            onSubmit={onSubmit}
           >
-            <Form>
-              <label htmlFor="firstName"></label>
-              <KodlamaIoTextInput id="firstName" name="firstName" placeholder="First name" />
-              <KodlamaIoTextInput name="lastName" placeholder="Last name" />
-              <KodlamaIoTextInput name="identityNumber" placeholder="Identity number" />
-              <KodlamaIoTextInput name="email" placeholder="Email" />
-              <KodlamaIoTextInput name="birthYear" placeholder="Birth year" />
-              <KodlamaIoTextInput name="password" placeholder="Password" />
-              <KodlamaIoTextInput name="passwordRepeat" placeholder="Password repeat" />
-              <button type="submit" className="btn btn-success">Submit</button>
-            </Form>
+            {formik => (
+              <Form>
+                <label htmlFor="firstName">İsim</label>
+                <KodlamaIoTextInput name="firstName" id="firstName" placeholder="First name" value={formik.values.firstName} />
+                <label htmlFor="lastName">Soyisim</label>
+                <KodlamaIoTextInput name="lastName" id="lastName" placeholder="Last name" value={formik.values.lastName} />
+                <label htmlFor="lastName">TC Kimlik Numarası</label>
+                <KodlamaIoTextInput name="identityNumber" id="identityNumber" placeholder="Identity number" value={formik.values.identityNumber} />
+                <label htmlFor="lastName">Email</label>
+                <KodlamaIoTextInput name="email" id="email" placeholder="Email" value={formik.values.email} />
+                <label htmlFor="lastName">Doğum Yılı</label>
+                <KodlamaIoTextInput name="birthYear" id="birthYear" placeholder="Birth year" value={formik.values.birthYear} />
+                <label htmlFor="lastName">Şifre</label>
+                <KodlamaIoTextInput name="password" id="password" placeholder="Password" value={formik.values.password} />
+                <label htmlFor="lastName">Şifre Tekrar</label>
+                <KodlamaIoTextInput name="passwordRepeat" id="passwordRepeat" placeholder="Password repeat" />
+                <button type="submit" className="btn btn-success">Submit</button>
+              </Form>
+            )}
           </Formik>
         </div></div>
-    </div>
+    </div >
   )
 }
 
---
+++ src/pages/CandidateRegister/CandidateRegister.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CandidatesRegister from '../../components/CandidateRegister/CandidateRegister'
+
+function CandidateRegister() {
+  return (
+    <div className="container">
+      <CandidatesRegister />
+    </div>
+  )
+}
+
+export default CandidateRegister
