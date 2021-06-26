332770a158e2df8ca73671f66b3cb0323aa99d2b
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-27 02:21:51 +0300

    candidate component updated.

--- src/components/Candidate/Candidate.js
+++ src/components/Candidate/Candidate.js
@@ -2,13 +2,13 @@ import React from 'react'
 import userImage2 from '../../assets/images/userImages2.jpg'
 import './scss/Candidate.scss'
 
-function Candidate() {
+function Candidate({ candidate }) {
   return (
-    <div className="col-sm mt-5">
+    <div className="col-12 col-sm-6 col-lg-4 mt-5">
       <div className="card">
         <div className="card-body">
           <img className="card-img-top" src={userImage2} alt="userImage" />
-          <h5 className="card-text mt-2 fw-bold">First name Last Name</h5>
+          <h5 className="card-text mt-2 fw-bold">{candidate.firstName} {candidate.lastName}</h5>
           <div className="d-flex justify-content-between">
             <button className="btn btn-success">Cv File download</button>
             <button className="btn btn-primary">Cv İncele</button>
