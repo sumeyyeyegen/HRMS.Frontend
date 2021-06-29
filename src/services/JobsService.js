d6b95f4b6ae699996334b701cbfa98a952e53ab5
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-29 22:29:41 +0300

    jobService to jobsService

--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const JOBS_REST_API_URL = "http://localhost:8080/api/jobs/"
 
-export default class JobService {
+export default class JobsService {
   getAllJobs() {
     return axios.get(`${JOBS_REST_API_URL}getall`);
   }
