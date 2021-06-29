772fbb56c4000f422bbcbc0bd7a7135bd6d82f64
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-29 04:56:12 +0300

    candidateReducer added to rootReducer.

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,11 @@
 //tüm reducer'ları(stateleri) bir yerde toplamak için kullanılır.
 
-// import { combineReducers } from "redux";
+import { combineReducers } from "redux";
+import candidateReducer from "./reducers/candidateReducer";
 
 
-// const rootReducer = combineReducers({
+const rootReducer = combineReducers({
+  candidates: candidateReducer
+})
 
-// })
-
-// export default rootReducer;
+export default rootReducer;
