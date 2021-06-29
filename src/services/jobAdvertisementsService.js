2b56ff695c35770e6cca32a94c8fb650a84af705
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-29 04:59:59 +0300

    addJobAdvert added to jobAdvertisementsService.

--- src/services/jobAdvertisementsService.js
+++ src/services/jobAdvertisementsService.js
@@ -3,6 +3,25 @@ import axios from 'axios'
 const JOB_ADVERTİSEMENTS_REST_API_URL = "http://localhost:8080/api/jobadvertisements/"
 
 export default class JobAdvertisementsService {
+
+  addJobAdvert(data) {
+    return axios({
+      method: "POST",
+      url: `${JOB_ADVERTİSEMENTS_REST_API_URL}add`,
+      data: {
+        jobId: data.jobId,
+        cityId: data.cityId,
+        employerId: data.employerId,
+        jobDescription: data.jobDescription,
+        active: data.active,
+        minSalary: data.minSalary,
+        maxSalary: data.maxSalary,
+        openPositions: data.openPositions,
+        releaseDate: data.releaseDate,
+        applicationDeadline: data.applicationDeadline
+      }
+    })
+  }
   getActiveJobAdvertisements() {
     return axios.get(`${JOB_ADVERTİSEMENTS_REST_API_URL}getByIsActiveTrue`);
   }
