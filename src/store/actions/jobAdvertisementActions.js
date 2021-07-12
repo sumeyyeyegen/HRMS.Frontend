a8856d10f15ac47ed2e1e012d71a92ee75c1a06c
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-12 04:47:54 +0300

    jobAdvertActions changed.

--- src/store/actions/jobAdvertisementActions.js
+++ src/store/actions/jobAdvertisementActions.js
@@ -1,7 +1,16 @@
-// import JobAdvertisementsService from "../../services/JobAdvertisementsService";
-
+import JobAdvertisementsService from "../../services/JobAdvertisementsService"
+export const GET_JOB_ADVERTİSEMENT = "GET_JOB_ADVERTİSEMENTS"
 export const ADD_JOB_ADVERTISEMENT = "ADD_JOB_ADVERTISEMENT"
 export const DELETE_JOB_ADVERTISEMENT = "DELETE_JOB_ADVERTISEMENT"
+
+const jobAdvertisementService = new JobAdvertisementsService();
+
+export function getJobAdvertisements(jobAdvert) {
+  return {
+    type: GET_JOB_ADVERTİSEMENT,
+    payload: jobAdvert
+  }
+}
 
 export function addJobAdvertisement(jobAdvert) {
   return {
@@ -10,9 +19,16 @@ export function addJobAdvertisement(jobAdvert) {
   }
 }
 
+// pardon ya şimdi anladım bekle şurda kullancaz
 export function deletejobAdvertisement(jobAdvert) {
   return {
     type: DELETE_JOB_ADVERTISEMENT,
     payload: jobAdvert
+  }
+}
+
+export function getJobAdvertisementApi() {
+  return async (dispath) => {
+    await jobAdvertisementService.getActiveJobAdvertisements().then(res => dispath(getJobAdvertisements(res.data.data)))
   }
 }
