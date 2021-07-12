9f4f7f69f757eaf92b62a42d51dd2d5779e147c1
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-12 04:44:23 +0300

    configureStore changed.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+import thunk from "redux-thunk"
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
 
 export function configureStore() {
-  return createStore(rootReducer, devToolsEnhancer());
+  return createStore(rootReducer, composedEnhancer);
 }
