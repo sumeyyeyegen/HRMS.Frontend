829e080b059213e65f4dc1b8dce075085838f4f2
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-12 04:42:33 +0300

    auth state added to rootReducer.

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,13 +1,14 @@
 //tüm reducer'ları(stateleri) bir yerde toplamak için kullanılır.
 
 import { combineReducers } from "redux";
+import authReducer from "./reducers/authReducer";
 import candidateReducer from "./reducers/candidateReducer";
 import jobAdvertisementReducer from "./reducers/jobAdvertisementReducer";
 
-
 const rootReducer = combineReducers({
   candidates: candidateReducer,
-  jobAdvertisements: jobAdvertisementReducer
+  jobAdvertisements: jobAdvertisementReducer,
+  auth: authReducer
 })
 
 export default rootReducer;
