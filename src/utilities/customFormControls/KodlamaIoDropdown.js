741dc7411c9f734848b6034209974b64da24da22
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-12 04:44:03 +0300

    kodlamaIoDropdown completed.

--- src/utilities/customFormControls/KodlamaIoDropdown.js
+++ src/utilities/customFormControls/KodlamaIoDropdown.js
@@ -1,23 +1,18 @@
 import React from 'react'
 
-function KodlamaIoDropdown({ label, id, handleChange, value, states, errors, touched, description }) {
+function KodlamaIoDropdown({ onChange, value, states, label, name, ...props }) {
   return (
     <div className="mb-3">
-      <label htmlFor={id} className="w-25">{description}</label>
-      <select
-        id={id}
-        value={value}
-        onChange={handleChange}
-        className="form-select"
-      >
-        <option value={0} label={label} />
-        {
-          states && states.map(state => <option value={state.id} label={state.name || state.title} key={state.id} />)
-        }
-        {errors.id && touched.id && (
-          <div className="input-feedback">{errors.id}</div>
-        )}
+      <label htmlFor={name}>{label}</label>
+      <select name={name} onChange={onChange} className="form-select" value={value} {...props}>
+        <option value="0">{label} Seç..</option>
+        {states.length && states.map(s => {
+          return <option key={s.id} value={s.id}> {s.title || s.name} </option>
+        })}
       </select>
+      {/* {meta.touched && meta.error ? (
+        <span className="custom-input-error">{meta.error.value}</span>
+      ) : null} */}
     </div>
   )
 }
