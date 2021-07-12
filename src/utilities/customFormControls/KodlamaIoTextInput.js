5c3d596beabc4cbd819e832db9d12f5e51708cb1
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-12 04:43:42 +0300

    kodlamaIoTextInput class updated.

--- src/utilities/customFormControls/KodlamaIoTextInput.js
+++ src/utilities/customFormControls/KodlamaIoTextInput.js
@@ -4,13 +4,10 @@ import { useField } from 'formik'
 function KodlamaIoTextInput({ name, label, ...props }) {
   //reflect api
   const [field, meta] = useField(name);
-
-  console.log(field);
-  console.log(meta);
   return (
     <div className="mb-3">
       <label htmlFor={name}>{label}</label>
-      <input {...field} {...props} className={`${meta.error && meta.touched ? "border-red-500" : ""} shodow appearance-none border rounded w-full form-control py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} />
+      <input {...field} {...props} className={`${meta.error && meta.touched ? "border-red-500" : ""} shodow appearance-none border w-full form-control py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} />
     </div>
   )
 }
