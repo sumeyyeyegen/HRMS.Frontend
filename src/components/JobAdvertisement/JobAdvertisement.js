fd813c5d5bd8a45f878e0c6ea1ba92c51d5d658a
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-21 03:24:19 +0300

    jobAdvert.js updated.

--- src/components/JobAdvertisement/JobAdvertisement.js
+++ src/components/JobAdvertisement/JobAdvertisement.js
@@ -4,21 +4,19 @@ import { Link } from 'react-router-dom'
 
 function JobAdvertisement({ jobAdvertisement }) {
   return (
-    <div className="col-lg-4 col-12">
-      <div className="card">
-        <div className="card-body">
-          <Link to="/jobAdvertDetail">
-            <h4>{jobAdvertisement.job.title}</h4>
-          </Link>
+    <div className="col-lg-4 col-md-6 col-12 mb-4" >
+      <div className="h-100 job-item p-4">
+        <Link to="/jobAdvertDetail">
+          <h4>{jobAdvertisement.job.title}</h4>
+        </Link>
 
-          <small className="text-muted">
-            <b>{jobAdvertisement.employer.companyName}</b> - {jobAdvertisement.employer.webSite}
-          </small>
-          <p>{jobAdvertisement.jobDescription}</p>
-          <small>
-            <div><i className="bi bi-geo-alt-fill"></i> {jobAdvertisement.city.name}  {jobAdvertisement.applicationDeadline}</div>
-          </small>
-        </div>
+        <small className="text-muted">
+          <b>{jobAdvertisement.employer.companyName}</b> - {jobAdvertisement.employer.webSite}
+        </small>
+        <p>{jobAdvertisement.jobDescription}</p>
+        <small>
+          <div><i className="bi bi-geo-alt-fill"></i> {jobAdvertisement.city.name}  {jobAdvertisement.applicationDeadline}</div>
+        </small>
       </div>
     </div>
   )
