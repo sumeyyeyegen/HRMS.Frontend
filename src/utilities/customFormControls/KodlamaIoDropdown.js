b233a82c9e145f907621bb45df0b2f40b9dc5087
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-21 03:21:24 +0300

    kodlamaIoDropdown updated.

--- src/utilities/customFormControls/KodlamaIoDropdown.js
+++ src/utilities/customFormControls/KodlamaIoDropdown.js
@@ -1,18 +1,20 @@
+import { useField } from 'formik';
 import React from 'react'
 
 function KodlamaIoDropdown({ onChange, value, states, label, name, ...props }) {
+  const [field, meta] = useField(name);
   return (
     <div className="mb-3">
       <label htmlFor={name}>{label}</label>
-      <select name={name} onChange={onChange} className="form-select" value={value} {...props}>
+      <select name={name} onChange={onChange} {...field} className={`form-select ${meta.error && meta.touched ? "border-red-500" : ""} shodow appearance-none border w-full form-control py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} value={value} {...props}>
         <option value="0">{label} Seç..</option>
         {states.length && states.map(s => {
           return <option key={s.id} value={s.id}> {s.title || s.name} </option>
         })}
       </select>
-      {/* {meta.touched && meta.error ? (
+      {meta.touched && meta.error ? (
         <span className="custom-input-error">{meta.error.value}</span>
-      ) : null} */}
+      ) : null}
     </div>
   )
 }
